7e971549df9ba65fa0455971d685cadb0703b931
Kashif <kashifansari9625@gmail.com>
2025-12-18 18:28:24 +0530

    Fix Header runtime error by removing undefined daysSinceExam usage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import HeaderStats from './HeaderStats';
-
+import React from "react";
+import HeaderStats from "./HeaderStats";
 
 /**
  * Header Component
- * Displays the main application title
- * Fully responsive & semantic
+ * - Layout + branding only
+ * - NO business logic
  */
 const Header = () => {
   return (
     <header className="header">
-  <div className="header-main">
-    {/* existing title / subtitle */}
-  </div>
+      <div className="header-main">
+        {/* existing title / subtitle */}
+      </div>
 
-  <HeaderStats />
-</header>
-
+      <HeaderStats />
+    </header>
   );
 };
-const daysElapsed = daysSinceExam(EXAM_DATE);
 
 export default Header;
